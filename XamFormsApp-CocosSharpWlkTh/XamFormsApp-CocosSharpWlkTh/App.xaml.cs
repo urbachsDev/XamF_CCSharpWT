d59f6f8fc64d3bf11030b28515e60bc93603b110
urbachsDev <67059833+urbachsDev@users.noreply.github.com>
2020-06-28 12:06:04 -0700

    Add project files.

---
+++ XamFormsApp-CocosSharpWlkTh/XamFormsApp-CocosSharpWlkTh/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamF_CCSharpWT.Services;
+using XamF_CCSharpWT.Views;
+
+namespace XamF_CCSharpWT
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ XamFormsApp-CocosSharpWlkTh/XamFormsApp-CocosSharpWlkTh/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamF_CCSharpWT.Models;
+
+namespace XamF_CCSharpWT.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
